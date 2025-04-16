b79fc985c8f1438e8381be428db3a947c2dbdc2e
David Parker <parkers.postbox@gmail.com>
2025-04-16 05:24:31 +0100

    Rough Outline

--- typing-speed/main.py
+++ typing-speed/main.py
@@ -3,6 +3,10 @@ from tkinter import ttk
 from ctypes import windll
 windll.shcore.SetProcessDpiAwareness(1)
 
+from pprint import pprint
+
+WORD_FILE = 'typing-speed/test_file.txt'
+
 class TypingSpeedTest(tk.Tk):
   def __init__(self, screenName = None, baseName = None, className = "Tk", useTk = True, sync = False, use = None) -> None:
     super().__init__(screenName, baseName, className, useTk, sync, use)
@@ -12,12 +16,67 @@ class TypingSpeedTest(tk.Tk):
     # Hide the root window until centered on screen
     self.withdraw()
 
+    self._load_words()
+    self._prepare_words()
+    self.bind('<Key>', self._key_press)
+
     # Create the GUI
-    ################
+    # word_display = ttk.Label(self, text=self._word_list[0])
+    # word_display.pack()
+
+    word_len = len(self._word_list[2])
+    char_spacing = 0.02
+    for idx in range(word_len):
+      label = ttk.Label(self, text=self._word_list[2][idx])
+      label.place(relx=0.5+(idx*char_spacing), rely=0.2, anchor=tk.CENTER)
     
     # Show the window
     self._center_window()
     self.deiconify()
+
+
+  # Load words from file into a list
+  def _load_words(self) -> None:
+    try:
+      with open(WORD_FILE) as file:
+        # Flatten the files into a list of words
+        self._word_list = [word for words in file.readlines() for word in words.split()]
+
+    except FileNotFoundError as ex:
+      print(f'{WORD_FILE}: {ex.strerror}')
+
+
+  def _prepare_words(self) -> None:
+    self._num_words = len(self._word_list)
+    self._current_word = self._word_list[0]
+    print(f'Word file has: {self._num_words} words. Current word is {self._current_word}.')
+
+  
+  # Callback from tk logging key presses
+  def _key_press(self, ev: tk.Event) -> None:
+    match ev.keysym:
+      case 'Shift_L':
+        return
+      
+      case 'Shift_R':
+        return
+      
+      case 'Return':
+        # Maybe special use case?
+        return
+      
+      case 'Control_L':
+        return
+      
+      case 'Control_R':
+        return
+      
+      case 'space':
+        # Maybe special use case?
+        return
+
+      case _:
+        print(ev.keysym)
 
 
   # Window config functions
